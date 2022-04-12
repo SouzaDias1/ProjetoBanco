ca4283a770718028469d071a1961c293378b456b
Pedro Henrique Gonçalves de Souza <phdias2004@gmail.com>
2022-04-12 17:21:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Conta C1 = new Conta (1);
+Conta C2 = new Conta (2);
+
+C1.Depositar (1000.00);
+C1.Sacar (200.00);
+Console.WriteLine(C1.Saldo);
+
+C1.Depositar (200.00);
+C1.Sacar (150.00);
+Console.WriteLine(C2.Saldo);
